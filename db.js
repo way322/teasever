965c549007a286e2d1fcc0bb709afbbefb06a4e4
WAY <iliyamalihin@yandex.ru>
2025-06-30 18:53:21 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,13 @@
+// db.js
 import pg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT, 10),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
